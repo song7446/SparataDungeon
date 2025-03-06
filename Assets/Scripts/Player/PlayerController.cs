222aa81988e4f80b63f71c6e98c68411818f3e0e
song7446 <songwon4529@naver.com>
2025-03-06 15:07:35 +0900

    Add : 플레이어 이동 및 점프 추가

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +9,17 @@ public class PlayerController : MonoBehaviour
     Vector3 curInput;
     Rigidbody _rigidbody;
     [SerializeField] private float _speed = 3f;
+    [SerializeField] private float jumpPower = 3f;
+    [SerializeField] private LayerMask roadLayerMask;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+
     }
 
     private void FixedUpdate()
@@ -37,5 +45,20 @@ public class PlayerController : MonoBehaviour
         {
             curInput = Vector3.zero;
         }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && OnGround())
+        {
+            _rigidbody.AddForce(Vector2.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+
+    bool OnGround()
+    {
+        Ray ray = new Ray(transform.position + (-transform.up * 0.4f), Vector3.down);
+        Debug.Log(ray);
+        return Physics.Raycast(ray, 0.1f, roadLayerMask);
     }
 }
