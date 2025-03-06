8bae58853b11ebe1877343e94a0d82b76f8f9e51
song7446 <songwon4529@naver.com>
2025-03-06 16:35:21 +0900

    Add : 카메라 움직임 추가

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,25 +6,38 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Move")]
     Vector3 curInput;
     Rigidbody _rigidbody;
     [SerializeField] private float _speed = 3f;
     [SerializeField] private float jumpPower = 3f;
     [SerializeField] private LayerMask roadLayerMask;
 
+    [Header("Look")]
+    [SerializeField] private Transform cameraContainer;
+    Vector2 mouseDelta;
+    float cameraRot;
+    [SerializeField] private float lookSensitivity;
+    [SerializeField] private float minCameraX;
+    [SerializeField] private float maxCameraX;
+
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    private void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void FixedUpdate()
     {
         Move();
+    }
+    private void LateUpdate()
+    {
+        CameraLook();
     }
 
     void Move()
@@ -60,5 +73,19 @@ public class PlayerController : MonoBehaviour
         Ray ray = new Ray(transform.position + (-transform.up * 0.4f), Vector3.down);
         Debug.Log(ray);
         return Physics.Raycast(ray, 0.1f, roadLayerMask);
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    void CameraLook()
+    {
+        cameraRot += mouseDelta.y * lookSensitivity;
+        cameraRot = Mathf.Clamp(cameraRot, minCameraX, maxCameraX);
+        cameraContainer.localEulerAngles = new Vector3(-cameraRot, 0, 0);
+
+        transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
     }
 }
