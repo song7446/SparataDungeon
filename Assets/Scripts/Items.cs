fb316a79e237f964944882c66664605e2189807a
song7446 <songwon4529@naver.com>
2025-03-10 19:53:46 +0900

    Refactor : 아이템 지속시간 재는 함수 리팩토링

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Items : MonoBehaviour, InteractableObject
@@ -31,13 +32,40 @@ public class Items : MonoBehaviour, InteractableObject
             // 점프력 증가 
             case ConsumableType.Jump:
                 CharacterManager.Instance.Player.playerState.GetJump(itemData.consumables[0]);
-                UIManager.Instance.TimeCheck(itemData.consumables[0]);
+                TimeCheck(itemData.consumables[0]);
                 break;
             // 스피드 증가 
             case ConsumableType.Speed:
                 CharacterManager.Instance.Player.playerState.GetSpeed(itemData.consumables[0]);
-                UIManager.Instance.TimeCheck(itemData.consumables[0]);
+                TimeCheck(itemData.consumables[0]);
                 break;
         }
+    }
+
+    // 아이템 제한 시간용 
+    public void ChangeItemTime(float time)
+    {
+        UIManager.Instance.itemTime.text = time.ToString("N2");
+    }
+
+    public void TimeCheck(ItemDataConcumable itemData)
+    {
+        UIManager.Instance.itemTime.gameObject.SetActive(true);
+        StartCoroutine(StopWatch(itemData));
+    }
+
+    // 아이템의 지속시간부터 0까지 시간 재고 비활성화 코루틴
+    IEnumerator StopWatch(ItemDataConcumable itemData)
+    {
+        float time = itemData.time;
+        while (time > 0f)
+        {
+            time -= Time.deltaTime;
+            ChangeItemTime(time);
+            yield return null;
+        }
+        CharacterManager.Instance.Player.playerState.returnPlus?.Invoke();
+        UIManager.Instance.itemTime.gameObject.SetActive(false);
+        yield return null;
     }
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -41,31 +41,4 @@ public class UIManager : MonoBehaviour
         }
         itemTime.gameObject.SetActive(false);
     }
-
-    // 아이템 제한 시간용 
-    public void ChangeItemTime(float time)
-    {
-        itemTime.text = time.ToString("N2");
-    }
-
-    public void TimeCheck(ItemDataConcumable itemData)
-    {
-        itemTime.gameObject.SetActive(true);
-        StartCoroutine(StopWatch(itemData));        
-    }
-
-    // 아이템의 지속시간부터 0까지 시간 재고 비활성화 코루틴
-    IEnumerator StopWatch(ItemDataConcumable itemData)
-    {
-        float time = itemData.time;
-        while (time > 0f)
-        {
-            time -= Time.deltaTime;
-            ChangeItemTime(time);
-            yield return null;
-        }
-        CharacterManager.Instance.Player.playerState.returnPlus?.Invoke();
-        itemTime.gameObject.SetActive(false);
-        yield return null;
-    }
 }
